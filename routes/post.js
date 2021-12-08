0b610e3a4fb1591c3359a59c7c27f16904556363
Jainisha Parmar <jainishaparmar@Amits-MacBook-Pro.local>
2021-12-08 14:50:10 +0530

    commi

--- routes/post.js
+++ routes/post.js
@@ -3,7 +3,5 @@ const postController=require('../controllers/post')
 
 const router =express.Router()
 router.get('/',postController.getPosts);
-// exports.getPosts=(req,res)=>{
-//     res.send("hey");
-// }
+
 module.exports=router;
