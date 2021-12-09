80705b746ca9583f6e4be33581dbc5c68e3d7dc7
Jainisha Parmar <jainishaparmar@Amits-MacBook-Pro.local>
2021-12-09 15:13:43 +0530

    commit

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ const date = require('date-and-time')
 const swaggerUI=require('swagger-ui-express'),
     swaggerDocument=require('./swagger.json');
 
+const yaml = require('yamljs');
+const fs   = yaml.load('./swagger.yaml');
+
 app.use(morgan("dev"));
 app.use("/",postRoutes);
-app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDocument));
+app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(fs));
 app.get('/', (req,res) => {
     res.send("hey")
 });
