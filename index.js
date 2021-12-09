491096677f48b5daeec7a0d30fa49e24bdf91c49
Jainisha Parmar <jainishaparmar@Amits-MacBook-Pro.local>
2021-12-09 16:20:29 +0530

    doc:update file

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const morgan=require("morgan");
 const postRoutes=require("./routes/post");
 const date = require('date-and-time')
 const swaggerUI=require('swagger-ui-express'),
-    swaggerDocument=require('./swagger.json');
+    
 
-const yaml = require('yamljs');
-const fs   = yaml.load('./swagger.yaml');
+const swaggerYaml = require('yamljs');
+const swaggerDoc   = swaggerYaml.load('./swagger.yaml');
 
 
 app.use(morgan("dev"));
 app.use("/",postRoutes);
-app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(fs));
+app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDoc));
 app.get('/', (req,res) => {
     res.send("hey")
 });
