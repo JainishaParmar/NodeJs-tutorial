1af988117a9c4a442010f4efaba3a5fee7b13621
Jainisha Parmar <jainishaparmar@Amits-MacBook-Pro.local>
2021-12-13 10:22:03 +0530

    docs:husky modified

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-const express=require('express')
-const morgan = require("morgan");
+const express = require('express');
+
 const swaggerUI = require('swagger-ui-express');
 const swaggerYaml = require('yamljs');
 const postRoutes = require("./routes/post");
 
 const app = express();
-const swaggerDoc  = swaggerYaml.load('./swagger.yaml');
+const swaggerDoc = swaggerYaml.load('./swagger.yaml');
 
-app.use(morgan("dev"));
-app.use("/",postRoutes);
-app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDoc));
-app.get('/', (req,res) => {
-    res.send("hey")
-});
+app.use("/", postRoutes);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
 app.listen(8080);
