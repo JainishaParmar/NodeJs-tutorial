f4ef29eb0b08aebcc69b53b5d3b30fff6e51bc35
Jainisha Parmar <jainishaparmar@Amits-MacBook-Pro.local>
2021-12-13 13:56:47 +0530

    docs:file updated

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ const express = require('express');
 
 const swaggerUI = require('swagger-ui-express');
 const swaggerYaml = require('yamljs');
-const postRoutes = require("./routes/post");
 
 const app = express();
 const swaggerDoc = swaggerYaml.load('./swagger.yaml');
 
-app.use("/", postRoutes);
 app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
 app.listen(8080);
