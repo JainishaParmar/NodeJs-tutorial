87a5e1b81042ae59783a8e206cd71570adf7525a
JainishaParmar <parmarjainisha3300@gmail.com>
2021-12-15 15:27:34 +0530

    docs:file updated

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
+import mangoose from 'mongoose';
 
 import swaggerUI from 'swagger-ui-express';
 import swaggerYaml from 'yamljs';
@@ -11,6 +12,55 @@ import indexRouter from './routes/index';
 import usersRouter from './routes/users';
 
 const dirname = path.resolve();
+
+mangoose
+  .connect("mongodb://localhost:27017/tutorials", {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("Connection successfull!!"))
+  .catch((err) => console.log);
+
+// Schema
+const tutorialSchema = new mangoose.Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  published: {
+    type: Boolean,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+});
+
+// collection creation (model)
+const Tutorial = new mangoose.model("Tutorial", tutorialSchema);
+
+// create Document or insert
+const createDocument = async () => {
+  try {
+    const demoTutorial = new Tutorial({
+      id: "1",
+      published: true,
+    });
+
+    const result = await demoTutorial.save();
+    console.log(result);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+createDocument();
 
 const app = express();
 const swaggerDoc = swaggerYaml.load('./swagger.yaml');
